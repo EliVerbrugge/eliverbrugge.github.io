889806220f690cc719a1eb40e544b4f3b1db178b
Eli Verbrugge <eli.verbrugge@gmail.com>
2016-04-20 22:54:08 -0400

    Maybe done?

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,5 @@
 /*
+@license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
 The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
@@ -20,7 +21,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    app.baseUrl = '/EliVerbrugge.github.io/';
+    // app.baseUrl = '/polymer-starter-kit/';
   }
 
   app.displayInstalledToast = function() {
