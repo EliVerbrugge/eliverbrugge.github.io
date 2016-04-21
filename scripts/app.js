921758beff874fa5f93c8a65cc27d56e5205dc44
Eli Verbrugge <eli.verbrugge@gmail.com>
2016-04-20 21:24:33 -0400

    fixes

--- scripts/app.js
+++ scripts/app.js
@@ -16,7 +16,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   var app = document.querySelector('#app');
   
   // Sets app default base URL
-  app.baseUrl = '/';
+  app.baseUrl = '/EliVerbrugge.github.io/
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
